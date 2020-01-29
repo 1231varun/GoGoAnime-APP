61bae345b214a6297db1a26e52d7016ac894e954
Varun K Nair <varunnai@cisco.com>
2020-01-29 14:40:36 +0530

    remove by id

--- app/src/main/java/com/survivalstudiosentertainment/gogoanime/MainActivity.java
+++ app/src/main/java/com/survivalstudiosentertainment/gogoanime/MainActivity.java
@@ -228,7 +228,7 @@ public class MainActivity extends Activity {
 
             // hide element by class name
             webView.loadUrl("javascript:(function() { " +
-                    "document.getElementsByClassName('_gc2zius').style.display='none'; })()");
+                    "document.getElementsById('_ea90odf_2397038')[0].style.display='none'; })()");
             webView.loadUrl("javascript:(function() { " +
                     "document.getElementsByClassName('banner_center')[0].style.display='none'; })()");
             webView.loadUrl("javascript:(function() { " +
